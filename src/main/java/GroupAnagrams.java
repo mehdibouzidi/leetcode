69313601535fcac6f7a5a9e4b8f4ae52fd04f6af
Mehdi BOUZIDI <mehdi.bouzidi.92@gmail.com>
2022-10-21 13:58:20 +0200

    Solution to GroupAnagrams

---
+++ src/main/java/GroupAnagrams.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        groupAnagrams(strs).stream().forEach(System.out::println);
+    }
+    public  static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> strsMap = new HashMap<>();
+
+        for(int i=0; i<strs.length;i++){
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String ordredWord = new String(chars);
+            List<String> anagrams = strsMap.get(ordredWord);
+            if(anagrams==null || anagrams.size()==0){
+                anagrams = new ArrayList<>();
+            }
+            anagrams.add(strs[i]);
+            strsMap.put(ordredWord, anagrams);
+        }
+
+        return new ArrayList<>(strsMap.values());
+    }
+}
